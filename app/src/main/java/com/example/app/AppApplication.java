2c84da471b225bf9a6cccf058ffcccd323bee8e3
aleksandarjovanov <aleksandarjovanovv@gmail.com>
2022-09-20 00:36:28 +0200

    add reactive user api

--- app/src/main/java/com/example/app/AppApplication.java
+++ app/src/main/java/com/example/app/AppApplication.java
@@ -2,8 +2,13 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
 
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableReactiveMongoRepositories
 public class AppApplication {
 
     public static void main(String[] args) {
